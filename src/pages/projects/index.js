2133ce82d9ffdab04f49aee92c736008a51c37e1
hanhvu <hanhvuquoc@gmail.com>
2022-07-05 22:44:37 +0900

    feat: add placeholder text

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { Container, Heading } from "@chakra-ui/react";
 
 export default function Projects() {
   return (
     <Layout>
-      <div>Projects</div>
+      <Container
+        pt="60px"
+      >
+        <Heading as="h1">Project</Heading>
+      </Container>
     </Layout>
   );
 }
